4a5c2c518c2980f86ab8099dc14c2f6b7a1092b8
pinokio2003 <pinokio0476@gmail.com>
2023-12-03 20:46:12 +0200

    Initial Commit

---
+++ Task_One_Text_Animation/Task_One_Text_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_One_Text_AnimationApp.swift
+//  Task_One_Text_Animation
+//
+//  Created by Anatolii Kravchuk on 03.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Task_One_Text_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
