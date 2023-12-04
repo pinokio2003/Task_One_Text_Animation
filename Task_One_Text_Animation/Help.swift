5cbfd7e29123169b577feae74b5060cbd1656c00
pinokio2003 <pinokio0476@gmail.com>
2023-12-04 18:12:48 +0200

    Task: text with animation

---
+++ Task_One_Text_Animation/Help.swift
@@ -0,0 +1,8 @@
+//
+//  Help.swift
+//  Task_One_Text_Animation
+//
+//  Created by Anatolii Kravchuk on 03.12.2023.
+//
+
+import Foundation
---
+++ Task_One_Text_Animation/View/TextAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  TextAnimation.swift
+//  Task_One_Text_Animation
+//
+//  Created by Anatolii Kravchuk on 03.12.2023.
+//
+
+import SwiftUI
+
+struct TextAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextAnimation()
+}
